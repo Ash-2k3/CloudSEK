95ac095ddd7881e496974959175ca3f4aded5252
Ash-2k3 <ash.me.cse@gmail.com>
2024-08-29 15:55:40 +0530

    Added new model for User and updates to Post and Comment to incorporate user id.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,22 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db  # Importing the db object from __init__.py
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    posts = db.relationship('Post', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='author', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,9 +26,12 @@ class Post(db.Model):
 
     # Relationship to Comment
     comments = db.relationship('Comment', backref='post', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
